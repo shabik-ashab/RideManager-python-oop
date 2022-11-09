bd1fba0b0b3852de3e70d904ca596bcae5e357a2
nub69 <shabik.ashab@gmail.com>
2022-11-09 20:30:01 +0600

    log_in method created

--- user.py
+++ user.py
@@ -10,5 +10,23 @@ class User:
         file.close()
         print(self.name, 'user created')
 
+    @staticmethod
+    def log_in(email, password):
+        stored_password = ''
+        with open('users.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                if email in line:
+                    stored_password = line.split(' ')[1]
+        
+        file.close()
+        hashed_password = hashlib.md5(password.encode()).hexdigest()
+        if hashed_password == stored_password:
+            print('valid user')
+        else:
+            print('invalid user')
+
 
 hero = User('Hero Alom', "hero@mail.com", '123')
+
+hero.log_in('hero@mail.com', '1234')

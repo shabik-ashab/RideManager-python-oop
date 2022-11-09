63c56e7aaa36cb4879656cf12df623586946087a
nub69 <shabik.ashab@gmail.com>
2022-11-09 21:03:59 +0600

    BRTA initialized

---
+++ brta.py
@@ -0,0 +1,17 @@
+import random
+
+class BRTA:
+    def __init__(self) -> None:
+        self.__license = {}
+
+    def driving_test(self,email):
+        score = random.randint(0,100)
+        if score >= 33:
+            print("You have passed")
+            license_number = random.randint(5000,9999)
+            self.__license[email] = license_number
+            return license_number
+        else:
+            print("You failed")
+            return False
+    
